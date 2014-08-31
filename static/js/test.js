38c8bf51371b90cc0749f1b45890fe20176f006e
Aleksandrova <ahaa96@abv.bg>
2014-08-31 17:44:50 +0300

    touches

--- static/js/test.js
+++ static/js/test.js
@@ -2,18 +2,18 @@ console.log("sup");
 
 $("#button-products").click(function(){
 	$('html, body').animate({
-		scrollTop: $("#products-block").offset().top
-	}, 8000);
+		scrollTop: $("#products-block").offset().top  -50
+	}, 800);
 });
 
 $("#button-about").click(function(){
 	$('html, body').animate({
-		scrollTop: $("#about-block").offset().top
+		scrollTop: $("#about-block").offset().top -50
 	}, 1000);
 });
 
 $("#button-contact").click(function(){
 	$('html, body').animate({
-		scrollTop: $("#contact-block").offset().top
+		scrollTop: $("#contact-block").offset().top -50
 	}, 1100);
 });
